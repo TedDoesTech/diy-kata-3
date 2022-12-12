671c66711ee56e7cd66d406e18e156f5043416ac
Ed Crowther <edward930@hotmail.co.uk>
2022-12-12 17:18:13 +0000

    joinNames test and function completed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+const namesArray = namesObj.map((namesObj) => {
+    return `${namesObj.name}`
+})
+const namesString = namesArray.reduce((acc, curr, index) => {
+    if(index < namesArray.length-1) {
+        return `${acc}, ${curr}`  
+    } else {
+        return `${acc} & ${curr}`  
+    }
+})   
+return namesString;
+};
 
 module.exports = joinNames;
